d2ddba803cc3efea793e7b90df1b605f96275677
Thiago Cerutti <thiagocerutti@gmail.com>
2024-12-07 12:28:29 -0300

    feat: add express and initial routes

---
+++ api/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+
+app.get('/withQuery', (req, res) => {
+  res.send({
+    'route': 'withQuery',
+    'query': req.query
+  })
+})
+
+app.get('/withParams/:id', (req, res) => {
+  res.send({
+    'route': 'withParams',
+    'query': req.params
+  })
+})
+
+
+app.listen(port, () => {
+  console.log(`listening port ${port}`)
+})
+
