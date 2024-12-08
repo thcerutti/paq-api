18f7d3693c283e424ac44a0dc6b4001e4467c952
thcerutti <thiagocerutti@gmail.com>
2024-12-08 14:22:45 -0300

    feat: clean up swagger (for now)

--- api/educando.js
+++ api/educando.js
@@ -1,20 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-/**
- * @swagger
- * /educando/todos:
- *   get:
- *     summary: Retorna todos os educandos cadastrados
- *     tags:
- *      - Educandos
- *     responses:
- *       200:
- *         description: Lista de todos os educandos cadastrados
- *       404:
- *         description: Nenhum educando cadastrado
- */
-router.get("/todos", (req, res) => {
+router.get("/todos", (_, res) => {
   res.send([
     { id: 1, nome: "João", unidade: "SC401", trilha: "Programação" },
     { id: 2, nome: "Maria", unidade: "SC401", trilha: "Programação" },
@@ -22,27 +9,6 @@ router.get("/todos", (req, res) => {
   ]);
 });
 
-/**
- * @swagger
- * /educando/{id}:
- *   get:
- *     summary: Busca um educando pelo id
- *     description: Retorna um educando buscando por seu respectivo id
- *     tags:
- *       - Educandos
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *         description: ID do educando
- *     responses:
- *       200:
- *         description: Educando foi encontrado e retornado
- *       404:
- *         description: Educando não encontrado
- */
 router.get("/:id", (req, res) => {
   res.send({
     id: req.params.id,
@@ -50,6 +16,10 @@ router.get("/:id", (req, res) => {
     unidade: "SC401",
     trilha: "Programação",
   });
+});
+
+router.post("/conquista", (_, res) => {
+  res.status(201).send({ message: "Conquista cadastrada com sucesso" });
 });
 
 module.exports = router;
--- api/index.js
+++ api/index.js
@@ -2,71 +2,15 @@ const express = require("express");
 const app = express();
 const educandosRouter = require("./educando");
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.send({ message: "Bem vindo a API do PAQ" });
 });
 
-/**
- * @swagger
- * /withQuery:
- *   get:
- *     summary: Returns a greeting message
- *     responses:
- *       200:
- *         description: A greeting message
- *         content:
- *           text/plain:
- *             schema:
- *               type: string
- *               example: Hello world
- *
- */
-app.get("/withQuery", (req, res) => {
-  res.send({
-    route: "withQuery",
-    query: req.query,
-  });
-});
-
-/**
- * @swagger
- * /withParams/{id}:
- *   get:
- *     summary: Get with Params
- *     description: Returns a route with params
- *     parameters:
- *       - name: id
- *         in: path
- *         required: true
- *         type: string
- *     responses:
- *       200:
- *         description: Successful response
- *         schema:
- *           type: object
- *           properties:
- *             route:
- *               type: string
- *             query:
- *               type: object
- */
-app.get("/withParams/:id", (req, res) => {
-  res.send({
-    route: "withParams",
-    query: req.params,
-  });
-});
+app.use("/educando", educandosRouter);
 
 const port = 3001;
 app.listen(port, () => {
   console.log(`listening port ${port}`);
 });
-
-app.use("/educando", educandosRouter);
-
-const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("../swaggerConfig");
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 module.exports = app;
