da26332e0f5163790f034dabd3fc253bc1a57c57
thcerutti <thiagocerutti@gmail.com>
2024-12-09 09:03:06 -0300

    feat: implementa operações de criar, editar e excluir educando

--- src/models/Educando.js
+++ src/models/Educando.js
@@ -35,6 +35,57 @@ const Educando = {
       await client.close();
     }
   },
+  criar: async ({ nome, unidade, trilha, inicio }) => {
+    try {
+      await client.connect();
+      const res = await client
+        .db(process.env.MONGO_DB_DATABASE)
+        .collection("educando")
+        .insertOne({
+          nome,
+          unidade,
+          trilha,
+          inicio: new Date(inicio),
+        });
+      return res.insertedId;
+    } finally {
+      await client.close();
+    }
+  },
+  atualizar: async ({ id, nome, unidade, trilha, inicio }) => {
+    const dataToUpdate = {};
+    nome && (dataToUpdate.nome = nome);
+    unidade && (dataToUpdate.unidade = unidade);
+    trilha && (dataToUpdate.trilha = trilha);
+    inicio && (dataToUpdate.inicio = new Date(inicio));
+    try {
+      await client.connect();
+      const res = await client
+        .db(process.env.MONGO_DB_DATABASE)
+        .collection("educando")
+        .updateOne(
+          { _id: ObjectId.createFromHexString(id) },
+          {
+            $set: dataToUpdate,
+          }
+        );
+      return res.modifiedCount > 0;
+    } finally {
+      await client.close();
+    }
+  },
+  deletar: async (id) => {
+    try {
+      await client.connect();
+      const res = await client
+        .db(process.env.MONGO_DB_DATABASE)
+        .collection("educando")
+        .deleteOne({ _id: ObjectId.createFromHexString(id) });
+      return res.deletedCount > 0;
+    } finally {
+      await client.close();
+    }
+  },
 };
 
 module.exports = Educando;
