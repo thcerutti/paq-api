d388a2c7cab34899ae2cedb7fa9a0975a0d0d6c4
thcerutti <thiagocerutti@gmail.com>
2024-12-09 09:02:21 -0300

    feat: inclui `body-parser` no middleware

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const educandosRouter = require("../src/routes/educando");
 
@@ -8,6 +9,7 @@ app.get("/", (_, res) => {
   res.send({ message: "Bem vindo a API do PAQ" });
 });
 
+app.use(bodyParser.json());
 app.use("/educando", educandosRouter);
 
 const port = 3001;
